bbea76688246a702ed73526c747117aec7ea9fc3
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2020-12-20 16:37:14 +0200

    new try

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App: React.FunctionComponent = () => (
+    <div className="App">
+        <header>
+            <p>
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+        </header>
+    </div>
+)
+
+export default App
