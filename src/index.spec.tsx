f8741f9abf383df0ea4e731e1f343a03a4d796bc
Lauri Lavanti <lauri.lavanti@verkkokauppa.com>
2020-12-22 23:33:32 +0200

    test: 100% coverage

---
+++ src/index.spec.tsx
@@ -0,0 +1,30 @@
+import './logo.svg'
+
+const mockRender = jest.fn()
+
+jest.mock('react-dom', () => ({
+    __esModule: true,
+    default: {
+        ...jest.requireActual('react-dom'),
+        render: mockRender,
+    },
+}))
+
+describe('index', () => {
+    const mockGetElementById = jest.fn()
+    document.getElementById = mockGetElementById
+    it('should call ReactDOM.render with correct args', async () => {
+        await import('./index')
+        expect(mockGetElementById).toHaveBeenCalledWith('root')
+        expect(mockRender.mock.calls).toMatchInlineSnapshot(`
+      Array [
+        Array [
+          <UNDEFINED>
+            <App />
+          </UNDEFINED>,
+          undefined,
+        ],
+      ]
+    `)
+    })
+})
